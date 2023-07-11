d9d0aa61b27158741f5ed3e464461c580bace655
jbrizuela1976 <JavierBrizuela>
2023-07-11 18:14:58 -0300

    tercer commit

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 
 def login_view(request):
+    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -12,5 +13,13 @@ def login_view(request):
         
         if user:
             login(request, user)
+            messages.success(request, f'Bienvenido {user.username}')
             return redirect('index')
+        else:
+            messages.warning(request, 'su usuario o contraseña no coinciden')
     return render(request, 'users/login.html', {})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesion cerrada exitosamente')
+    return redirect('login')
