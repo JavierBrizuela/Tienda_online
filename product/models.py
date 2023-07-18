ac98d60994f3435341833585b7a80dfa8e932e98
javier brizuela <you@example.com>
2023-07-17 22:52:02 -0300

    agregando imagen al modelo

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
+import uuid
 
 # Create your models here.
 class Product(models.Model):
@@ -8,6 +9,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, default= 0.00)
     slug = models.SlugField(null=False, blank=False, unique=True)
+    image = models.ImageField(upload_to='product/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -19,6 +21,13 @@ class Product(models.Model):
         super(Product, self).save(*args, **kwargs) """
 
 def set_slug(sender, instance, *args, **kwargs): #callback
-        instance.slug = slugify(instance.title)
+        if instance.title and not instance.slug:
+            slug = slugify(instance.title)
+
+            while Product.objects.filter(slug=slug).exists():
+                slug = slugify(
+                      f'{instance.title} {str(uuid.uuid4())[:8]}'
+                )
+            instance.slug = slug
 
 pre_save.connect(set_slug, sender=Product)
