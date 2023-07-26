1a029468fc8118100eb2f3a880a713145641e879
javier brizuela <you@example.com>
2023-07-25 21:23:51 -0300

    add feature shoping carts

--- carts/models.py
+++ carts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from users.models import User
 from product.models import Product
+from django.db.models.signals import pre_save
+import uuid
 
 class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     product = models.ManyToManyField(Product)
     subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
@@ -10,4 +13,10 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return ''
+        return self.cart_id
+    
+def set_cart_id(sender, instance, *args, **kwargs):
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+pre_save.connect(set_cart_id, sender=Cart)
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Cart
+from product.models import Product
+from .utils import get_or_create_cart
 
-# Create your views here.
 def cart(request):
-    return render(request, 'carts/cart.html', {})
+    
+    cart = get_or_create_cart(request)
+
+    return render(request, 'carts/cart.html', {'cart':cart})
+
+def add(request):
+    
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    cart.product.add(product)
+
+    return render(request, 'carts/add.html', {'product':product})
