26cd0e00a1a141ca8dca1049fe6f49e3d173fb60
javier brizuela <you@example.com>
2023-08-08 00:06:30 -0300

    add snippets shipping_address

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import ShippingAddress
 from .forms import ShippingAddressForm
+from django.contrib import messages
 
 class ShippingAddressListView(ListView):
     model = ShippingAddress
@@ -16,8 +17,12 @@ def create(request):
     if request.method == 'POST' and form.is_valid:
         shipping_address = form.save(commit=False)
         shipping_address.user = request.user
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
         shipping_address.save()
-        
+
+        messages.success(request, 'La direccion se creo correctamente')
+        return redirect('shipping_addresses:shipping_addresses')
+
     return render(request,'shipping_addresses/create.html', {
         'form':form
     })
